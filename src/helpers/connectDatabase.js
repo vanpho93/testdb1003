1532808aed1af5b89f5f2c5d3517f96d7dc10a9e
Van Pho Nguyen <vanpho02@gmail.com>
2018-04-19 17:41:50 +0700

    mocha

--- src/helpers/connectDatabase.js
+++ src/helpers/connectDatabase.js
@@ -5,8 +5,9 @@ function getDatabaseUri() {
     if (process.env.NODE_ENV === 'test') return 'mongodb://localhost/mean1003-test';
     return 'mongodb://localhost/mean1003';
 }
+mongoose.Promise = global.Promise;
 
-mongoose.connect(getDatabaseUri())
+mongoose.connect(getDatabaseUri(), { useMongoClient: true })
 .then(() => console.log('Database connected.'))
 .catch(error => {
     console.log(error.message);
