79a67e3ecb164925bc304d19dff934a42cef0dd9
Van Pho Nguyen <vanpho02@gmail.com>
2018-04-24 17:42:14 +0700

    add refs

--- src/models/story.model.js
+++ src/models/story.model.js
@@ -3,7 +3,8 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const storySchema = new Schema({
-    content: { type: String, required: true, trim: true }
+    content: { type: String, required: true, trim: true },
+    author: { type: mongoose.Schema.Types.ObjectId, ref: 'User' }
 });
 
 const Story = mongoose.model('Story', storySchema);
--- src/models/user.model.js
+++ src/models/user.model.js
@@ -6,6 +6,7 @@ const userSchema = new Schema({
     email: { type: String, required: true, trim: true, unique: true },
     name: { type: String, required: true, trim: true },
     password: { type: String, required: true, trim: true },
+    stories: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Story' }]
 });
 
 const User = mongoose.model('User', userSchema);
