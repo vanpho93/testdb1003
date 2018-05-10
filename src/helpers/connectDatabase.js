b3e542d12eb9568c1c0fa43a6926083b3eb6e1b3
Van Pho Nguyen <vanpho02@gmail.com>
2018-05-10 18:37:05 +0700

    init

--- src/helpers/connectDatabase.js
+++ src/helpers/connectDatabase.js
@@ -1,10 +1,11 @@
 const mongoose = require('mongoose');
 
 function getDatabaseUri() {
-    if (process.env.NODE_ENV === 'production') return ''; 
+    if (process.env.NODE_ENV === 'production') return 'mongodb://admin:123@ds113169.mlab.com:13169/mean1003'; 
     if (process.env.NODE_ENV === 'test') return 'mongodb://localhost/mean1003-test';
     return 'mongodb://localhost/mean1003';
 }
+
 mongoose.Promise = global.Promise;
 
 mongoose.connect(getDatabaseUri(), { useMongoClient: true })
